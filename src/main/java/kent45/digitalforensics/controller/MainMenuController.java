b081779f3f599cfdb909d5fc0dfdc6ce323ca517
[jg684] <[jg684@kent.ac.uk]>
2023-12-05 16:34:10 +0000

    Added main menu controller

--- src/main/java/kent45/digitalforensics/controller/MainMenuController.java
+++ src/main/java/kent45/digitalforensics/controller/MainMenuController.java
@@ -4,11 +4,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller("/mainMenu")
+@Controller
 public class MainMenuController {
 
-    @GetMapping
+    @GetMapping("/mainMenu")
     public ModelAndView mainMenu() {
         return new ModelAndView("mainMenu");
+    }
+
+    @GetMapping("/startGame")
+    public ModelAndView startGame() {
+        return new ModelAndView("startGame");
+    }
+
+    @GetMapping("/leaderboard")
+    public ModelAndView leaderboard() {
+        return new ModelAndView("leaderboard");
+    }
+
+    @GetMapping("/tutorial")
+    public ModelAndView tutorial() {
+        return new ModelAndView("tutorial");
+    }
+
+    @GetMapping("/credits")
+    public ModelAndView credits() {
+        return new ModelAndView("credits");
     }
 }
