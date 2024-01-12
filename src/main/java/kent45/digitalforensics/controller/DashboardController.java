3d1847669740448c426d912f400824d52d087645
[jg684] <[jg684@kent.ac.uk]>
2024-01-12 12:59:25 +0000

    decision-screen: Added a decision screen

--- src/main/java/kent45/digitalforensics/controller/DashboardController.java
+++ src/main/java/kent45/digitalforensics/controller/DashboardController.java
@@ -5,6 +5,7 @@ import kent45.digitalforensics.service.DatabaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -25,5 +26,10 @@ public class DashboardController {
 
         return new ModelAndView("dashboard")
                 .addObject("scenario", scenario);
+    }
+
+    @GetMapping("/judgement")
+    public ModelAndView getJudgementScreen() {
+        return new ModelAndView("judgement");
     }
 }
